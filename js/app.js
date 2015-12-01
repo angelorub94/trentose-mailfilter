bbfb86f27ded9673aac8c491ba8507af4287659c
angelorub94 <angelo.rubino94@gmail.com>
2015-12-01 17:50:56 +0100

    fine primo sprint

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* your code should go here */
 
+var tmpl = '<li>MAIL</li>';
 
 // You can modify this object, add functions that you need
 var MailModel = {
@@ -17,10 +18,69 @@ var MailModel = {
     * @return an array of messages, excluding those that match the filter rules.
     */
     filter : function(){
-      return [];
+        var lista = [];
+        for(var i=0; i<this.messages.length;i++){
+        var flag = false;
+           for(var j=0; j<this.rules.length;j++){
+               if(this.messages[i].indexOf(this.rules[j])!=-1){
+                   flag = true;
+               }
+           }
+            if(flag == false){
+                lista[lista.length] = this.messages[i];
+            }
+        }      
+        return lista;
     }
 
   
+};
+
+var controller = {
+    init: function(){
+        MailModel.init();
+        view.renderAllMail();
+        view.clickFilter();
+    },
+    
+    getMsgslength: function(){
+        return MailModel.messages.length;
+    },
+    
+    getMsg: function(index){
+        return MailModel.messages[index];
+    },
+    
+    filter: function(){
+       var filtered = [];
+       filtered = MailModel.filter();
+       view.renderFilteredMail(filtered);
+    }
+};
+
+var view = {
+    
+    renderAllMail: function(){
+        for(var i=0; i<controller.getMsgslength(); i++){
+            var tmpl2 = tmpl.replace("MAIL", controller.getMsg(i));
+            $(".allmails").append(tmpl2);
+        }
+    },
+    
+    clickFilter: function(){
+        $(".btn-filter").click(function(){
+           controller.filter(); 
+        });
+    },
+    
+    renderFilteredMail: function(list){
+        $(".allmails").hide();
+        for(var i = 0; i< list.length ;i++){
+            var tmpl2 = tmpl.replace("MAIL", list[i]);
+            $(".result").append(tmpl2);
+        }
+        
+    }
 };
 
 // Example of usage:
@@ -35,5 +95,5 @@ var MailModel = {
 
 
 $(document).ready(function(){
-
+    controller.init();
 });
